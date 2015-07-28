f941989278ac08c3886d383e7f33bb9f994fbca2
Cris Silva Jr <ishiikurisu@yahoo.com>
2015-07-27 21:53:54 -0300

    Cleaning up stuff

--- ex/test.py
+++
@@ -1,8 +0,0 @@
-import os
-
-# os.system('g++ spojWILLITST.cpp -o spoj.exe -lm -O2')
-for T in xrange(100):
-    print T
-    os.system('echo %d | spojWILLITST.py' % (T))
-    #os.system('echo %d | spoj.exe' % (T))
-# os.system('del spoj.exe')
