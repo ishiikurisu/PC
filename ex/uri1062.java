b2f7c46b6a3d383a039a417c6a4db1bd39787763
Cris Silva Jr <ishiikurisu@yahoo.com>
2015-12-22 19:17:41 -0200

    Modified uri1062.java

--- ex/uri1062.java
+++ ex/uri1062.java
@@ -1,96 +1,57 @@
 import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.HashSet;
-import java.util.HashMap;
 
 public class uri1062
 {
     static InputStreamReader ISR = new InputStreamReader(System.in);
     static BufferedReader BR = new BufferedReader(ISR);
-
+    static int[] queue;
+    static int[] stack;
+    static int stackSize;
+    static int queueSize;
+    static boolean result;
     static int numberWagons;
-    static String referenceWagons;
-    static HashMap<String, HashSet> memo;
 
-    static boolean loop(LinkedList<Integer> station,
-                        LinkedList<Integer> railroad)
+    static private void dequeue()
     {
-        String stationString = station.toString();
-        String railString = railroad.toString();
+        
+    }
 
-        System.out.println("  " + stationString + "+" + railString);
+    static private void pop()
+    {
 
-        if (referenceWagons.equals(stationString))
-            return true;
-        if (memo.containsKey(stationString)) {
-            if (memo.get(stationString).contains(railString))
-                return false;
-            else
-                memo.get(stationString).add(railString);
-        }
-        else {
-            memo.put(stationString, new HashSet<String>());
-        }
+    }
 
-        LinkedList<Integer> newStation;
-        LinkedList<Integer> newRail;
-        boolean result = false;
+    static private void push()
+    {
 
-        if (railroad.size() > 0) {
-            newStation = new LinkedList<Integer>(station);
-            newRail = new LinkedList<Integer>(railroad);
-            newStation.add(0, newRail.remove(0));
-            result = loop(newStation, newRail);
-        }
-        if (result == true) return result;
-
-        if (station.size() > 0) {
-            newStation = new LinkedList<Integer>(station);
-            newRail = new LinkedList<Integer>(railroad);
-            newRail.offer(newStation.remove(0));
-            result = loop(newStation, newRail);
-        }
-
-        return result;
     }
 
     static void setup()
     throws IOException
     {
-        LinkedList<Integer> wagons = new LinkedList<Integer>();
+        String[] line = BR.readLine().split("\\s+");
+        queue = new int[numberWagons];
+        stack = new int[numberWagons];
+        stackSize = queueSize = 0;
+        result = true;
 
-        numberWagons = Integer.parseInt(BR.readLine());
-        if (numberWagons == 0) throw new IOException();
-
-        memo = new HashMap<String, HashSet>();
-
-        for (int w = 1; w <= numberWagons; ++w)
-            wagons.add(0, w);
-
+        for (int i = 0; i < numberWagons; ++i)
+            queue[i] = Integer.parseInt(line[i]);
     }
 
     static void draw()
     throws IOException
     {
-        String[] line = BR.readLine().split(" ");
-        int endOfLine = Integer.parseInt(line[0]);
-        LinkedList<Integer> station = new LinkedList<Integer>();
-        LinkedList<Integer> railroad = new LinkedList<Integer>();
+        while (wagon < numberWagons && result)
+        {
+            dequeue();
+            pop();
+            push()
+        }
 
-        if (endOfLine == 0)
-            throw new IOException();
-
-        for (int w = 0; w < numberWagons; ++w)
-            railroad.offer(Integer.parseInt(line[w]));
-        referenceWagons = railroad.toString();
-
-        railroad = new LinkedList<Integer>();
-        for (int w = 1; w <= numberWagons; ++w)
-            railroad.add(w);
-
-        if (loop(station, railroad))
+        if (result)
             System.out.println("Yes");
         else
             System.out.println("No");
@@ -99,21 +60,19 @@ public class uri1062
     public static void main(String[] args)
     throws IOException
     {
-        while (true)
+        numberWagons = Integer.parseInt(BR.readLine());
+
+        while (numberWagons != 0)
         {
             try {
                 setup();
-                while (true) {
-                    try { draw(); }
-                    catch (Exception end) {
-                        System.out.println();
-                        break;
-                    }
-                }
+                draw();
             }
             catch (Exception any) {
-                break;
+                System.out.println();
             }
+
+            numberWagons = Integer.parseInt(BR.readLine());
         }
 
         BR.close();
