500bc3229b40d40da25b11ecf3421c248ac19a7c
Cris Silva Jr <ishiikurisu@yahoo.com>
2016-04-17 12:44:27 -0300

    Adding some stuff I started before

---
+++ ex/uri1327.py
@@ -0,0 +1,26 @@
+def min_index(cards):
+    index, min = 0, cards[0]
+    for i, card in enumerate(cards):
+        if card < min:
+            index, min = i, card
+    return index
+
+def draw(players, cards):
+    if len(players) == 1:
+        return players[0]
+    else:
+        players.pop(min_index(cards[0:len(players)]))
+        return draw(players, cards[len(players):])
+
+def main():
+    if input() == 0: raise StandardError
+    players, cards = raw_input().split(' '), []
+    for _ in xrange(4):
+        for card in map(int, raw_input().split(' ')):
+            cards.append(card)
+    print draw(players, cards)
+
+if __name__ == '__main__':
+    while True:
+        try: main()
+        except: break
